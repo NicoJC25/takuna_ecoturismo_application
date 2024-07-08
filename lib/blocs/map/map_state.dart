8a9fd2d3f14a0aadf06d6054e27611ee646f450d
NicoJC25 <nejuez@misena.edu.co>
2024-07-08 16:57:28 -0500

    Commit Prototipo 1 totalmente funcional, cambiar aspecto visual de acuerdo a los estilos requeridos

--- lib/blocs/map/map_state.dart
+++ lib/blocs/map/map_state.dart
@@ -3,26 +3,40 @@ part of 'map_bloc.dart';
 class MapState extends Equatable {
   final bool isMapInitialized;
   final bool isFollowingRoute;
+  final bool isFollowingUser;
+  final bool showMyRoute;
 
   final Set<Polyline> polylines;
 
   const MapState({
     this.isMapInitialized = false,
-    this.isFollowingRoute = true,
+    this.isFollowingRoute = false,
+    this.isFollowingUser = true,
+    this.showMyRoute = true,
     Set<Polyline>? polylines,
   }) : polylines = polylines ?? const {};
 
   MapState copyWith({
     bool? isMapInitialized,
     bool? isFollowingRoute,
+    bool? isFollowingUser,
+    bool? showMyRoute,
     Set<Polyline>? polylines,
   }) =>
       MapState(
         isMapInitialized: isMapInitialized ?? this.isMapInitialized,
         isFollowingRoute: isFollowingRoute ?? this.isFollowingRoute,
+        isFollowingUser: isFollowingUser ?? this.isFollowingUser,
+        showMyRoute: showMyRoute ?? this.showMyRoute,
         polylines: polylines ?? this.polylines,
       );
 
   @override
-  List<Object> get props => [isMapInitialized, isFollowingRoute, polylines];
+  List<Object> get props => [
+        isMapInitialized,
+        isFollowingRoute,
+        isFollowingUser,
+        showMyRoute,
+        polylines
+      ];
 }
